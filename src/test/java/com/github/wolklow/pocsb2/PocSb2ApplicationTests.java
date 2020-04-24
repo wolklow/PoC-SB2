893cbc63dfb8d07b0412aad083bdcac5c4debc84
Orlando Vargas <6474589+ovargas@users.noreply.github.com>
2020-04-23 21:52:31 -0600

    feat: initial project template (#1)

---
+++ src/test/java/com/github/wolklow/pocsb2/PocSb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wolklow.pocsb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
